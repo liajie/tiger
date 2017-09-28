8c2df17976f62541526e3fd91f937372d729bb62
jiangshengxin <1348550820@qq.com>
2017-09-28 12:14:32 +0800

    2017-9-26 14:11:25

---
+++ my_project/src/components/dome.vue
@@ -0,0 +1,19 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+      message: ''
+    }
+  }
+}
+</script>
+
+<style>
+/*引入css文件
+@import '/static/css/easydialog.min.css';
+@import url("/static/assets/css/bootstrap.min.css");*/
+</style>
