3117f923d7b0873d3323b2fd1251a02c14e6c690
jiangshengxin <1348550820@qq.com>
2017-09-28 19:54:27 +0800

    2017-9-26 14:11:25

---
+++ advanced/frontend/controllers/IndexController.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace frontend\controllers;
+
+
+class IndexController extends CommonController
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * IndexController.php
+ *
+ * ...
+ *
+ * Copyright (c)2017 http://note.hanfu8.top
+ *
+ * 修改历史
+ * ----------------------------------------
+ * 2017/9/28, 作者: 降省心(QQ:1348550820), 操作:创建
+ **/
