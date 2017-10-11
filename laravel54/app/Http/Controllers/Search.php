b49a3ea5f817e61bb612222f8542bf8ee00dee20
jiangshengxin <1348550820@qq.com>
2017-10-11 20:56:54 +0800

    查询频道,header头搜索框修改

---
+++ laravel54/app/Http/Controllers/Search.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+class Search extends Controller
+{
+    //接值
+    public $data;
+    public function __construct()
+    {
+
+    }
+
+    //直播频道查询
+    public function search_find()
+    {
+        print_r()
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Search.php
+ *
+ * ...
+ *
+ * 2017 Copyright (c) http://note.hanfu8.top
+ *
+ * 修改历史
+ * ----------------------------------------
+ * 2017/10/11, 作者: 降省心(QQ:1348550820), 操作:创建
+ **/
